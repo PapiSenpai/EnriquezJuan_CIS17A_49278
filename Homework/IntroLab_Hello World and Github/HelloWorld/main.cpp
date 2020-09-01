8b285bad69b9a8f94c3866484874d018f7e46388
PapiSenpai <juan03192001@gmail.com>
2020-09-01 01:52:32 -0700

    Signed-off-by: PapiSenpai <juan03192001@gmail.com>

---
+++ Homework/IntroLab_Hello World and Github/HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   HelloWorld
+ * Author: Juan Enriquez
+ * 
+ * Created on September 1, 2020, 1:15 AM
+ */
+
+#include <iostream>
+using namespace std;
+
+/*
+ * This program will output "Hello World!"
+ */
+
+int main() 
+{
+    cout << "Hello World!";
+    
+    return 0;
+}
+
